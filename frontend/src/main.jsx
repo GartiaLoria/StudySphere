2eb2d0ad64fa632cd1cd6e24829ba9fb86398fb4
GartiaLoria <loriamgartia@gmail.com>
2025-03-28 17:37:27 +0530

    Merged main into frontend-loria and added new UI components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react'
+//import { Provider } from "@/components/ui/provider"
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,6 +7,8 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+  {/* <Provider> */}
     <App />
-  </BrowserRouter>,
+  {/* </Provider> */}
+  </BrowserRouter>
 )
